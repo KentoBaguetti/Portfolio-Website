424473f7a79f7300af4381111a050e2f74df80d7
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-06-07 17:20:54 -0400

    webp image added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import logo from "../assets/kenvlogo2.jpeg";
+import kenImg from "../assets/kenvlogo2.webp";
 import Type from "./Type";
 
 const Home = () => {
@@ -6,7 +7,7 @@ const Home = () => {
     <div className="home-container container col-xxl-8 px-4 py-5">
       <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
         <div className="col-10 col-sm-8 col-lg-6 mx-auto">
-          <img src={logo} className="zoomable-image" alt="Logo" />
+          <img src={kenImg} className="zoomable-image" alt="Boku da" />
         </div>
         <div className="col-lg-6 mx-auto">
           <h1 className="home-title display-2 lh-1 mb-3">Kentaro Barnes</h1>
