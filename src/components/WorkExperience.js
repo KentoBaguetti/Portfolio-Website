8507ac2ca030eddae8c00127558a1fb055b40c2e
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-06-15 14:49:51 -0400

    W.E. more info

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -7,9 +7,20 @@ const WorkExperience = () => {
         <p className="swe-role">
           Software Engineering Intern | Feb 2024 - Current
         </p>
-        <p className="job-desc">
-          🛠️ Database Development, Fullstack Development 💻
-        </p>
+        <ul className="job-desc">
+          <li>
+            Designed and implemented a MongoDB database to manage data processed
+            by RPA scripts 🛠️
+          </li>
+          <li>
+            Developed a frontend dashboard for company clients to visualize and
+            manage data from the database 💻
+          </li>
+          <li>
+            Implemented user authentication within the dashboard and database 🔒
+          </li>
+          <li>Debugged RPA scripts to improve efficiency and accuracy 🎯</li>
+        </ul>
       </div>
     </div>
   );
