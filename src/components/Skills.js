137f843d343e306c3705e69bb3ac7b4241892681
Kentaro Barnes <130931962+KentoBaguetti@users.noreply.github.com>
2024-07-08 00:19:44 -0400

    C++

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,32 +1,32 @@
 const Skills = () => {
-  return (
-    <div className="skills">
-      <h1 className="skills-title pi-title">Skills && Technologies</h1>
-      <ul className="skill-list">
-        <li>
-          <strong className="skill-header">Programming Languages: </strong>
-          Python, Typescript, Java, Javascript
-        </li>
-        <li>
-          <strong className="skill-header">Backend Technologies: </strong>Node,
-          Express, Axios, Restful APIs
-        </li>
-        <li>
-          <strong className="skill-header">UI Technologies: </strong>React,
-          HTML, CSS, Bootstrap
-        </li>
-        <li>
-          <strong className="skill-header">Database: </strong>MongoDB, Mongoose
-        </li>
-        <li>
-          <strong className="skill-header">Version Control: </strong>Git, Github
-        </li>
-        <li>
-          <strong className="skill-header">Testing: </strong>Mocha
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className="skills">
+			<h1 className="skills-title pi-title">Skills && Technologies</h1>
+			<ul className="skill-list">
+				<li>
+					<strong className="skill-header">Programming Languages: </strong>
+					Python, Typescript, C++, Java, Javascript
+				</li>
+				<li>
+					<strong className="skill-header">Backend Technologies: </strong>Node,
+					Express, Axios, Restful APIs
+				</li>
+				<li>
+					<strong className="skill-header">UI Technologies: </strong>React,
+					HTML, CSS, Bootstrap
+				</li>
+				<li>
+					<strong className="skill-header">Database: </strong>MongoDB, Mongoose
+				</li>
+				<li>
+					<strong className="skill-header">Version Control: </strong>Git, Github
+				</li>
+				<li>
+					<strong className="skill-header">Testing: </strong>Mocha
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Skills;
