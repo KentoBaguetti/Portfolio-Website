4303aa5afe9c143b8b327085dab062960541d83d
Kentaro Barnes <kentaro.barnes5@gmail.com>
2024-11-29 15:34:06 -0800

    fix home photo loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,18 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import Type from "./Type";
-
-const KenImg = lazy(() => import("../assets/IMG_5840.webp"));
+import KenImg from "../assets/IMG_5840.webp";
 
 const Home = () => {
   return (
     <div className="home-container container col-xxl-8 px-4 py-5">
       <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
         <div className="col-10 col-sm-8 col-lg-6 mx-auto">
-          <Suspense fallback={<div>Loading image...</div>}>
-            <img src={KenImg} className="zoomable-image" alt="Boku da" />
-          </Suspense>
+          <img
+            src={KenImg}
+            loading="lazy"
+            className="zoomable-image"
+            alt="Boku da"
+          />
         </div>
         <div className="col-lg-6 mx-auto">
           <h2 className="home-title-small display-4 lh-1 mb-3">Hello, I'm</h2>
