d1d82120797c4f08c765238a00624fc7e1404206
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-05-28 03:23:33 -0700

    hero should be done now

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,7 @@
 import Hero from "@/components/Hero";
 import { AuroraBackground } from "@/components/ui/aurora-background";
+import * as motion from "motion/react-client";
+import type { Variants } from "motion/react";
 
 export default function Home() {
 	return (
@@ -7,6 +9,14 @@ export default function Home() {
 			<div className="min-h-screen flex justify-center items-center">
 				<Hero />
 			</div>
+			<motion.div
+				initial="offscreen"
+				whileInView="onscreen"
+				viewport={{ once: true }}
+				className="min-h-screen flex justify-center items-center"
+			>
+				<Hero />
+			</motion.div>
 		</AuroraBackground>
 	);
 }
--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -2,28 +2,48 @@
 import React from "react";
 import Image from "next/image";
 import Tilt from "react-parallax-tilt";
+import TypeWriter from "typewriter-effect";
 
 export default function Hero() {
 	return (
 		<Tilt
 			tiltReverse={true}
-			tiltMaxAngleX={10}
-			tiltMaxAngleY={10}
+			tiltMaxAngleX={6}
+			tiltMaxAngleY={6}
 			transitionSpeed={1000}
-			perspective={1000}
-			className="mx-auto my-auto"
+			perspective={1500}
+			gyroscope={false}
+			trackOnWindow={true}
+			className="flex flex-auto justify-center items-center h-screen w-screen"
 		>
-			<div className="flex flex-row flex-auto justify-center items-center bg-gray-800 rounded-4xl">
+			<div className="flex flex-row flex-auto justify-center items-center border-solid border-4 border-gray-200 rounded-4xl shadow-2xl max-w-1/3">
 				<div className="m-3">
 					<Image
 						src="/ken.jpeg"
-						width={250}
-						height={250}
+						width={275}
+						height={275}
 						alt="Image of Kentaro Barnes"
 						className="rounded-4xl opacity-100"
 					/>
 				</div>
-				<div className="m-3 text-6xl font-bold">Kentaro Barnes</div>
+				<div className="m-3 text-gray-200">
+					<p className="text-2xl">Hi, I&#39;m</p>
+					<p className="text-6xl font-bold">Kentaro Barnes</p>
+					<div className="text-2xl">
+						<TypeWriter
+							options={{
+								strings: [
+									"Computer Science Student . . .",
+									"Software Engineer . . .",
+								],
+								autoStart: true,
+								loop: true,
+								deleteSpeed: 20,
+								delay: 60,
+							}}
+						/>
+					</div>
+				</div>
 			</div>
 		</Tilt>
 	);
