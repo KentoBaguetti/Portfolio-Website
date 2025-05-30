529e16cf43b83fb4cc49df01ad59e3d562610898
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-05-29 21:29:32 -0700

    save

--- client/src/components/Projects.tsx
+++ client/src/components/Projects.tsx
@@ -1,16 +1,15 @@
 "use client";
 import React, { useState, useEffect, useRef } from "react";
 import * as motion from "motion/react-client";
-import { AnimatePresence } from "motion/react";
 import { useInView } from "motion/react";
 
-const projectBoxStyle = "bg-indigo-500 border-2 rounded-3xl p-3";
+const projectBoxStyle = "border-gray-200 border-4 rounded-3xl p-3";
 
 export default function Projects() {
 	const [isMobile, setIsMobile] = useState(false);
 
 	const ref = useRef(null);
-	const isInView = useInView(ref, { amount: "all" });
+	const isInView = useInView(ref, { amount: 0.0, margin: "-300px" });
 
 	useEffect(() => {
 		console.log(`User is ${isInView ? "in" : "not in"} view`);
@@ -25,39 +24,66 @@ export default function Projects() {
 		<div className="text-gray-200 min-h-screen min-w-screen flex justify-center items-center ">
 			<div
 				ref={ref}
-				className="relative mx-auto grid h-60 w-10/12 place-content-center"
+				className="relative mx-auto grid h-60 w-11/12 place-content-center"
 			>
 				<motion.div
-					className={`absolute bottom-0 left-0 top-0 w-[18%] ${projectBoxStyle}`}
-					animate={{ y: isInView ? "-80%" : "0%" }}
+					className={`absolute bottom-0 left-[41%] top-0 w-[18%] ${projectBoxStyle}`}
+					animate={{
+						y: isInView ? "-80%" : "0%",
+						x: isInView ? "-120%" : "0%",
+						opacity: isInView ? 1 : 0,
+					}}
+					whileHover={{ scale: 1.2 }}
 				>
 					Box 1
 				</motion.div>
 				<motion.div
-					className={`absolute bottom-0 left-[20.5%] top-0 w-[18%] ${projectBoxStyle}`}
-					animate={{ y: isInView ? "80%" : "0%" }}
+					className={`absolute bottom-0 left-[41%] top-0 w-[18%] ${projectBoxStyle}`}
+					animate={{
+						y: isInView ? "80%" : "0%",
+						x: isInView ? "-120%" : "0%",
+						opacity: isInView ? 1 : 0,
+					}}
+					whileHover={{ scale: 1.2 }}
 				>
 					Box 2
 				</motion.div>
 				<motion.div
 					className={`absolute bottom-0 left-[41%] top-0 w-[18%] ${projectBoxStyle}`}
-					animate={{ y: isInView ? "-120%" : "0%" }}
+					animate={{
+						y: isInView ? "-80%" : "0%",
+						x: isInView ? "120%" : "0%",
+						opacity: isInView ? 1 : 0,
+					}}
+					whileHover={{ scale: 1.2 }}
 				>
 					Box 3
 				</motion.div>
 				<motion.div
-					className={`absolute bottom-0 left-[61.5%] top-0 w-[18%] ${projectBoxStyle}`}
-					animate={{ y: isInView ? "80%" : "0%" }}
+					className={`absolute bottom-0 left-[41%] top-0 w-[18%] ${projectBoxStyle}`}
+					animate={{
+						y: isInView ? "80%" : "0%",
+						x: isInView ? "120%" : "0%",
+						opacity: isInView ? 1 : 0,
+					}}
+					whileHover={{ scale: 1.2 }}
 				>
 					Box 4
 				</motion.div>
 				<motion.div
-					className={`absolute bottom-0 left-[82%] top-0 w-[18%] ${projectBoxStyle}`}
-					animate={{ y: isInView ? "-80%" : "0%" }}
+					className={`absolute bottom-0 left-[41%] top-0 w-[18%] ${projectBoxStyle} flex flex-col flex-auto justify-center items-center text-3xl`}
+					whileHover={{ scale: 1.2 }}
 				>
-					Box 5
+					Featured Projects
 				</motion.div>
 			</div>
 		</div>
 	);
+}
+
+interface projectDetails {
+	name: string;
+	githubLink: string;
+	shortDesc: string;
+	techStack: string[];
 }
