69d0604e07173781a0b75c2452e81e40aa1d1481
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-05-29 20:47:40 -0700

    start of projects

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 import About from "@/components/About";
 import Experience from "@/components/Experience";
+import Projects from "@/components/Projects";
 
 export default function Home() {
 	return (
@@ -12,6 +13,9 @@ export default function Home() {
 			<div className="min-h-screen flex flex-col justify-center items-center px-4 gap-8">
 				<About />
 				<Experience />
+			</div>
+			<div className="">
+				<Projects />
 			</div>
 		</AuroraBackground>
 	);
--- client/src/components/Projects.tsx
+++ client/src/components/Projects.tsx
@@ -1,5 +1,63 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+import * as motion from "motion/react-client";
+import { AnimatePresence } from "motion/react";
+import { useInView } from "motion/react";
+
+const projectBoxStyle = "bg-indigo-500 border-2 rounded-3xl p-3";
 
 export default function Projects() {
-	return <div>Projects</div>;
+	const [isMobile, setIsMobile] = useState(false);
+
+	const ref = useRef(null);
+	const isInView = useInView(ref, { amount: "all" });
+
+	useEffect(() => {
+		console.log(`User is ${isInView ? "in" : "not in"} view`);
+	}, [isInView]);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia("(max-width: 768px)");
+		setIsMobile(mediaQuery.matches);
+	}, []);
+
+	return (
+		<div className="text-gray-200 min-h-screen min-w-screen flex justify-center items-center ">
+			<div
+				ref={ref}
+				className="relative mx-auto grid h-60 w-10/12 place-content-center"
+			>
+				<motion.div
+					className={`absolute bottom-0 left-0 top-0 w-[18%] ${projectBoxStyle}`}
+					animate={{ y: isInView ? "-80%" : "0%" }}
+				>
+					Box 1
+				</motion.div>
+				<motion.div
+					className={`absolute bottom-0 left-[20.5%] top-0 w-[18%] ${projectBoxStyle}`}
+					animate={{ y: isInView ? "80%" : "0%" }}
+				>
+					Box 2
+				</motion.div>
+				<motion.div
+					className={`absolute bottom-0 left-[41%] top-0 w-[18%] ${projectBoxStyle}`}
+					animate={{ y: isInView ? "-120%" : "0%" }}
+				>
+					Box 3
+				</motion.div>
+				<motion.div
+					className={`absolute bottom-0 left-[61.5%] top-0 w-[18%] ${projectBoxStyle}`}
+					animate={{ y: isInView ? "80%" : "0%" }}
+				>
+					Box 4
+				</motion.div>
+				<motion.div
+					className={`absolute bottom-0 left-[82%] top-0 w-[18%] ${projectBoxStyle}`}
+					animate={{ y: isInView ? "-80%" : "0%" }}
+				>
+					Box 5
+				</motion.div>
+			</div>
+		</div>
+	);
 }
