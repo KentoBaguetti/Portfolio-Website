01d158fc8df8b86dafa429232ea378a0b83b6be6
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-05-31 01:45:25 -0700

    bezels

--- client/src/components/ui/iPadBackground.tsx
+++ client/src/components/ui/iPadBackground.tsx
@@ -4,9 +4,12 @@ export default function IPadBackground({children, className}: {children: React.R
   return (
     <div className="h-screen w-screen flex justify-center items-center">
         {/* the actual ipad outer border */}
-      <div className="h-10/12 w-full md:h-10/12 md:w-9/12 border-solid border-black border-8 rounded-4xl">
-        {/*  */}
-        <div></div>
+      <div className="h-10/12 w-full md:h-11/12 md:w-10/12 border-solid border-black bg-black border-8 rounded-4xl flex justify-center items-center">
+        {/* ipad screen */}
+        <div className='h-10/12 w-11/12 md:h-11/12 md:min-w-11/12 rounded-3xl bg-blue-500 flex justify-center items-center'>
+        {/* main content */}
+        <main className={className}>{children}</main>
+        </div>
       </div>
     </div>
   )
