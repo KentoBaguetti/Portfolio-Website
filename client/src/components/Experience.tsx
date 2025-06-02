46adac9ba0a8cc9093d70f1778942c6f111a4620
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-06-02 04:22:00 -0700

    experience update

--- client/src/components/Experience.tsx
+++ client/src/components/Experience.tsx
@@ -63,14 +63,24 @@ export default function Experience() {
 								<p className="text-sm">Feb 2024 - Jun 2024</p>
 								<div>
 									<ul className="list-disc pl-6">
-											<li>Worked on data entry automation tools that were 85% faster than the industry standard</li>
-											<li>Designed and deployed a Mongo database, integrated into the existing infrastructure</li>
-											<li>Developed a secure front-end dashboard allowing customers to view their data securely</li>
-											<li>Created parsing algorithms to make onboarding new customers more efficient</li>
-											<li>Integrated an over the phone AI agent that parsed customer data and sent it directly to our automation service</li>
-											<li>Reduced real-time audio translation costs by implementing a redis database cache</li>
+											<li>Researched for viable database solutions to be integrated with the current infrastructure</li>
+											<li>Debugged automation tools</li>
+											<li>Debugged ML algorithms</li>
 									</ul>
-									</div>
+								</div>
+								</div>
+								<br />
+								<div>
+								<p className="text-lg font-bold">Student | UBC</p>
+								<p className="text-sm">Sep 2024 - Present</p>
+								<p>Yea, I go to skool. Fun time.</p>
+								<div>
+									<ul className="list-disc pl-6">
+											<li>Attended classes</li>
+											<li>Ate a lot of pizza</li>
+											<li>Persevered through harsh exam conditions and optimized my thought to action speeds by 200%</li>
+									</ul>
+								</div>
 								</div>
 							</div>
 						</div>
