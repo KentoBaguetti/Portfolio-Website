2ebbc74da827af298f70c6e55fdcac8e89c26ae2
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-06-04 14:23:48 -0700

    make experience easier to scale

--- client/src/components/Experience/Experience.tsx
+++ client/src/components/Experience/Experience.tsx
@@ -10,18 +10,39 @@ export default function Experience() {
       ref={ref}
       className="relative ml-[10%] w-full h-auto md:h-screen p-0 md:p-2 flex flex-col items-center justify-center space-y-8"
     >
-      <ExperienceCard
-        position="Software Engineer"
-        company="Consign AI"
-        workPeriod="Jun 2024 - Mar 2025"
-        summary="Developed AI automation tools, built full-stack applications, worked on real-time over the phone AI translations."
-      />
-      <ExperienceCard
-        position="Software Engineering Intern"
-        company="Consign AI"
-        workPeriod="Feb 2024 - Jun 2024"
-        summary="Worked on AI automation tools to improve data entry efficiency by 85% for clients."
-      />
+      {experienceArr.map((thingy, index) => (
+        <ExperienceCard
+          key={index}
+          position={thingy.position}
+          company={thingy.company}
+          workPeriod={thingy.workPeriod}
+          summary={thingy.summary}
+        />
+      ))}
     </div>
   );
 }
+
+interface experienceObj {
+  position: string;
+  company: string;
+  workPeriod: string;
+  summary: string;
+}
+
+const experienceArr: experienceObj[] = [
+  {
+    position: "Software Engineer",
+    company: "Consign AI",
+    workPeriod: "Jun 2024 - Mar 2025",
+    summary:
+      "Developed AI automation tools, built full-stack applications, worked on real-time over the phone AI translations.",
+  },
+  {
+    position: "Software Engineering Intern",
+    company: "Consign AI",
+    workPeriod: "Feb 2024 - Jun 2024",
+    summary:
+      "Worked on AI automation tools to improve data entry efficiency by 85% for clients.",
+  },
+];
