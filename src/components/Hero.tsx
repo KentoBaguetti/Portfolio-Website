17b172e212aed7c76541297b19d0d14cb88f9fa2
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-07-20 01:58:08 -0700

    tf these chagesm

---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen font-sans">
+      <h1 className="text-4xl font-bold font-sans">Hello, I'm Kentaro</h1>
+      <p className="text-lg font-sans">
+        I'm a software engineer with a passion for building web applications.
+      </p>
+    </div>
+  );
+}
