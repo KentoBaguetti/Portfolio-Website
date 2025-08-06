1787ae37ba1169090e3b3638d55fe73d0b66a106
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-08-06 14:42:28 -0700

    grammar

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,19 +12,19 @@ export default function About() {
         <h2 className="text-4xl mb-5 self-start font-bold">About Me</h2>
         <div className="flex flex-row w-full text-2xl text-tron-grey">
           <ul>
-            <li>I'm a third year CS student at UBC</li>
+            <li>I'm a third year CS student at UBC.</li>
             <li>
               Most of my experience is in{" "}
-              <span className="text-tron-yellow">full stack development</span>
+              <span className="text-tron-yellow">full stack development</span>.
             </li>
             <li>
               I'm interested in{" "}
               <span className="text-tron-yellow">Backend Development</span> and{" "}
-              <span className="text-tron-yellow">AI + Machine Learning</span>
+              <span className="text-tron-yellow">AI + Machine Learning</span>.
             </li>
             <li>
               My interests include programming, football, snowboarding, and much
-              more
+              more.
             </li>
             <li>
               Feel free to <span className="text-tron-yellow">reach out</span>{" "}
