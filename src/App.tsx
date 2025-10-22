14aec9a96307162b44aab974cdc77a493c948cbb
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-10-22 00:12:54 -0700

    mac push

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Hero from "./components/Hero";
 import Experience from "./components/Experience";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="flex flex-col justify-center w-full overflow-x-hidden">
+      {/* <div className="z-50 fixed">
+        <Navbar />
+      </div> */}
+      <Navbar />
       <Hero />
       <Experience />
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,60 +1,16 @@
+import styles from "../styles/Navbar.module.css";
 import { useState } from "react";
 
-export default function Navbar({
-  handleSelectedCardFromNavbar,
-}: {
-  handleSelectedCardFromNavbar: (card: string) => void;
-}) {
-  const [selectedCard, setSelectedCard] = useState<string>("About");
-  const selectedLinkStyles =
-    "text-lg sm:text-3xl md:text-2xl font-sans text-tron-yellow cursor-pointer font-bold hover:scale-110";
-  const unselectedLinkStyles =
-    "text-lg sm:text-3xl md:text-2xl font-sans text-tron-blue cursor-pointer hover:scale-110";
+export default function Navbar() {
+  const [activeButton, setActiveButton] = useState("Home");
 
   return (
-    <div className="w-full flex flex-row items-center gap-2 sm:gap-4 md:gap-8overflow-x-auto pb-2">
-      <div>
-        <h2
-          className={
-            selectedCard === "About" ? selectedLinkStyles : unselectedLinkStyles
-          }
-          onClick={() => {
-            handleSelectedCardFromNavbar("About");
-            setSelectedCard("About");
-          }}
-        >
-          ~/About
-        </h2>
-      </div>
-      <div>
-        <h2
-          className={
-            selectedCard === "Experience"
-              ? selectedLinkStyles
-              : unselectedLinkStyles
-          }
-          onClick={() => {
-            handleSelectedCardFromNavbar("Experience");
-            setSelectedCard("Experience");
-          }}
-        >
-          ~/Experience
-        </h2>
-      </div>
-      <div>
-        <h2
-          className={
-            selectedCard === "Projects"
-              ? selectedLinkStyles
-              : unselectedLinkStyles
-          }
-          onClick={() => {
-            handleSelectedCardFromNavbar("Projects");
-            setSelectedCard("Projects");
-          }}
-        >
-          ~/Projects
-        </h2>
+    <div className={`${styles.wrapper}`}>
+      <div className={`${styles.navbar}`}>
+        <button className={`${styles.nb_bubble}`}>Home</button>
+        <button className={`${styles.nb_bubble}`}>About</button>
+        <button className={`${styles.nb_bubble}`}>Experience</button>
+        <button className={`${styles.nb_bubble}`}>Projects</button>
       </div>
     </div>
   );
