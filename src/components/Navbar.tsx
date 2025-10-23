42065f065e9298dcc9dc562c55f6964ff9ce1c49
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-10-23 15:53:18 -0700

    add scrolling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import styles from "../styles/Navbar.module.css";
 import { useState } from "react";
+import { Link } from "react-scroll";
 
 export default function Navbar() {
   const [activeButton, setActiveButton] = useState("Home");
@@ -7,30 +8,39 @@ export default function Navbar() {
   return (
     <div className={`${styles.wrapper}`}>
       <div className={`${styles.navbar}`}>
-        <button
-          className={`${styles.nb_bubble} ${
-            activeButton === "Home" ? styles.nb_bubble_selected : ""
-          }`}
-          onClick={() => setActiveButton("Home")}
-        >
-          Home
-        </button>
-        <button
-          className={`${styles.nb_bubble} ${
-            activeButton === "About" ? styles.nb_bubble_selected : ""
-          }`}
-          onClick={() => setActiveButton("About")}
-        >
-          About
-        </button>
-        <button
-          className={`${styles.nb_bubble} ${
-            activeButton === "Experience" ? styles.nb_bubble_selected : ""
-          }`}
-          onClick={() => setActiveButton("Experience")}
-        >
-          Experience
-        </button>
+        <Link to="hero" smooth={true} duration={600}>
+          <button
+            className={`${styles.nb_bubble} ${
+              activeButton === "Home" ? styles.nb_bubble_selected : ""
+            }`}
+            onClick={() => setActiveButton("Home")}
+          >
+            Home
+          </button>
+        </Link>
+
+        <Link to="about" smooth={true} offset={0} duration={600}>
+          <button
+            className={`${styles.nb_bubble} ${
+              activeButton === "About" ? styles.nb_bubble_selected : ""
+            }`}
+            onClick={() => setActiveButton("About")}
+          >
+            About
+          </button>
+        </Link>
+
+        <Link to="experience" smooth={true} offset={-90} duration={600}>
+          <button
+            className={`${styles.nb_bubble} ${
+              activeButton === "Experience" ? styles.nb_bubble_selected : ""
+            }`}
+            onClick={() => setActiveButton("Experience")}
+          >
+            Experience
+          </button>
+        </Link>
+
         <button
           className={`${styles.nb_bubble} ${
             activeButton === "Projects" ? styles.nb_bubble_selected : ""
