79ab3ee4a83cd4bf9f4233684720f8ac7f2f1168
Kentaro Barnes <kentaro.barnes5@gmail.com>
2025-10-27 02:12:14 -0700

    add projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,105 +1,117 @@
-// import { motion } from "motion/react";
+import { motion } from "motion/react";
 import styles from "../styles/ProjectsStyling.module.css";
+import { FaGithub } from "react-icons/fa";
 
-// const projects = [
-//   {
-//     name: "GO Web Crawler",
-//     link: "https://github.com/KentoBaguetti/Web-Crawler-GO",
-//     basicDescription:
-//       "Web Crawler that searches for more URLs based on an inital URL and given keywords",
-//   },
-//   {
-//     name: "Cournot Game",
-//     link: "https://cournot-game.vercel.app/",
-//     basicDescription: "Multiplayer cournot competition simulator",
-//   },
-//   {
-//     name: "Sonura",
-//     link: "https://github.com/marlotea/sonura",
-//     basicDescription:
-//       "Spotify Music Recommendation System + Playlist Generator",
-//   },
-//   {
-//     name: "Shuukan",
-//     link: "https://github.com/KentoBaguetti/Shuukan",
-//     basicDescription:
-//       "Mobile app to help schedule studying sessions + habits during exam season",
-//   },
-//   {
-//     name: "Debate Bot",
-//     link: "https://github.com/KentoBaguetti/Debate-Bot",
-//     basicDescription:
-//       "Bot made with the OpenAI api whose sole purpose is to rage bait you",
-//   },
-//   {
-//     name: "Maze Solver",
-//     link: "https://github.com/KentoBaguetti/Maze-Solver",
-//     basicDescription: "Maze solver with Dijkstras, Backtracking BFS, and DFS",
-//   },
-// ];
+const projects = [
+  {
+    name: "GO Web Crawler",
+    link: "https://github.com/KentoBaguetti/Web-Crawler-GO",
+    basicDescription:
+      "Web Crawler that scrapes web pages for URLs based on keywords and in parallel with the worker pool pattern and breadth first search.",
+    technologies: ["Go", "net/http", "sync"],
+  },
+  {
+    name: "Sonura",
+    link: "https://github.com/marlotea/sonura",
+    basicDescription:
+      "Spotify Music Recommendation System + Playlist Generator. Allows users to swipe for songs like Tinder and generate playlists based on their preferences.",
+    technologies: ["Python", "Spotipy", "FastAPI", "NextJS"],
+  },
+  {
+    name: "Shuukan",
+    link: "https://github.com/KentoBaguetti/Shuukan",
+    basicDescription:
+      "Mobile app to help schedule studying sessions + habits during exam season. Tracks user's progress and provides reminders as well as a pomodoro timer.",
+    technologies: ["React Native", "TypeScript", "Expo"],
+  },
+  {
+    name: "Debate Bot",
+    link: "https://github.com/KentoBaguetti/Debate-Bot",
+    basicDescription:
+      "Bot made with the OpenAI api whose sole purpose is to rage bait you.",
+    technologies: ["TypeScript", "OpenAI API", "React", "Express"],
+  },
+];
 
 export default function Projects() {
   return (
     <div
-      className={`overflow-hidden py-40 ${styles.main_container} flex flex-col justify-center items-center font-japanese text-2xl text-center`}
+      className={`overflow-hidden py-40 ${styles.main_container} flex flex-col justify-center items-center text-2xl text-center w-full`}
     >
-      <div>Projects - Coming Soon!</div>
-      <div>
-        Check out my{" "}
-        <a
-          href="https://www.github.com/KentoBaguetti"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline underline-offset-4 text-blue-700"
-        >
-          github
-        </a>{" "}
-        in the meantime!
+      <div className="w-1/2 flex flex-col gap-4 justify-center items-center">
+        <div className={`${styles.header} mb-10`}>Some of my projects...</div>
+        <div className={`w-2/3 flex flex-col gap-8`}>
+          {projects.map((project) => {
+            return (
+              <ProjectItem
+                name={project.name}
+                link={project.link}
+                basicDescription={project.basicDescription}
+                technologies={project.technologies}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
 }
 
-//  <motion.div
-//         initial={{ opacity: 0, y: 100 }}
-//         animate={{ opacity: 1, y: 0 }}
-//         transition={{ duration: 0.5 }}
-//         className="flex flex-col w-full py-16"
-//       >
-//         <h2 className="text-4xl mb-5 self-start font-bold">Projects</h2>
-//         <div className="w-full">
-//           {projects.map((project) => (
-//             <ProjectItem
-//               name={project.name}
-//               link={project.link}
-//               basicDescription={project.basicDescription}
-//             />
-//           ))}
-//         </div>
-//       </motion.div>
+const ProjectItem = ({
+  name,
+  link,
+  basicDescription,
+  technologies,
+}: {
+  name: string;
+  link: string;
+  basicDescription?: string;
+  technologies?: string[];
+}) => {
+  return (
+    <motion.div
+      variants={{
+        hidden: { opacity: 0, y: 100 },
+        visible: { opacity: 1, y: 0 },
+      }}
+      whileHover={{ scale: 1.05 }}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{ duration: 0.5 }}
+      className={`${styles.project_item} flex flex-col items-center justify-center`}
+    >
+      <div className="flex flex-row w-10/12 gap-8 justify-center items-center">
+        <div className={`text-2xl font-bold ${styles.title}`}>{name}</div>
+        <motion.button
+          whileHover={{ scale: 1.2 }}
+          transition={{ duration: 0.5 }}
+        >
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mx-3"
+          >
+            <FaGithub size={25} className="md:w-[30px] md:h-[30px]" />
+          </a>
+        </motion.button>
+      </div>
 
-// const ProjectItem = ({
-//   name,
-//   link,
-//   basicDescription,
-// }: {
-//   name: string;
-//   link: string;
-//   basicDescription?: string;
-// }) => {
-//   return (
-//     <div className="flex flex-row justify-between items-center w-full mb-1 py-1">
-//       <div className="text-lg text-tron-grey">
-//         <a
-//           className="text-tron-yellow underline-offset-2 no-underline hover:underline"
-//           rel="noopener noreferrer"
-//           target="_blank"
-//           href={link}
-//         >
-//           {name}
-//         </a>
-//         {` - ${basicDescription}`}
-//       </div>
-//     </div>
-//   );
-// };
+      <div className="text-left text-sm">{basicDescription}</div>
+      <div className="flex flex-row gap-4 w-10/12 justify-center items-center mt-6">
+        {technologies?.map((tech) => (
+          <TechItem name={tech} />
+        ))}
+      </div>
+    </motion.div>
+  );
+};
+
+const TechItem = ({ name }: { name: string }) => {
+  return (
+    <div className={`${styles.tech_item} text-xs`}>
+      <div>{name}</div>
+    </div>
+  );
+};
