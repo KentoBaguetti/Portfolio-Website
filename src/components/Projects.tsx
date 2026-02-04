1365bf649cc3b686fc8d6d00ea407a4476943420
Kentaro Barnes <kentaro.barnes5@gmail.com>
2026-02-03 19:38:11 -0800

    update transitions

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -44,7 +44,7 @@ export default function Projects() {
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
-          viewport={{ once: true }}
+          viewport={{ once: false }}
           className={`${styles.header} mb-6 md:mb-10`}
         >
           Some of my projects...
@@ -54,7 +54,7 @@ export default function Projects() {
           style={{ perspective: 1000 }}
           initial="hidden"
           whileInView="visible"
-          viewport={{ once: true, amount: 0.1 }}
+          viewport={{ once: false, amount: 0.1 }}
           variants={{
             hidden: { opacity: 0 },
             visible: {
