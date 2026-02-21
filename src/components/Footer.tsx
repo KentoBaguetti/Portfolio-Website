fb1b3d264a23e6ad7a3735cdbeb95232cde63a85
Kentaro Barnes <kentaro.barnes5@gmail.com>
2026-02-20 23:36:06 -0800

    remove email

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div
       className={`flex flex-row justify-center items-end min-h-80 pb-5 font-extrabold ${styles.footer_background} text-center`}
     >
-      <div>Developed by Kentaro Barnes - kentaro.barnes5@gmail.com</div>
+      <div>Developed by Kentaro Barnes - 2026</div>
     </div>
   );
 }
